af16e26b4303cb49dbafb4f0f093fe2b571f1ef3
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-10-09 13:12:01 +0300

    add UIImagePickerController

--- Description/ViewController.swift
+++ Description/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var descriptionImage: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
     }
 
+    
+    @IBAction func libraryBarButtonPressed(_ sender: UIBarButtonItem) {
+        // activation method imagePickerController
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 
+}
+// MARK: - UIImagePickerControllerDelegate, UINavigationControllerDelegate
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // get original image
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        // set image by default on the screen
+        descriptionImage.image = image
+        
+        // avoid freezing app
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        
+    }
 }
 
